3420d78bc5bf6d7ee17563cb60b5335d8c89ba3a
oz28 <oz28@cornell.edu>
2019-10-27 20:22:11 -0400

    added files and specs and all the other things?

---
+++ game.mli
@@ -0,0 +1,31 @@
+(** 
+   Controls all game functions.
+
+   This module handles all scoring, music, and game data.
+*)
+
+open Main
+
+(** The type of values representing the positions of the icons. *)
+type matrix
+
+(** The abstact type of values representing game state. *)
+type t 
+
+(** Sets the number of players for this game. *)
+val set_number_players : string -> int
+
+(** Calculates score for current state. *)
+val score : t -> int
+
+(** [is_hit c t] is whether or not the player tap is accurate. 
+    A tap is accurate if it is hit at the right time and position. *)
+val is_hit : Tap.t -> t -> bool
+
+(** [update t] is the updated game state at this beat. [update] is called each
+    beat.*)
+val update : t -> t
+
+(** [update_matrix t] is the updated matrix, updated the rows for each beat. 
+    randomizes new icon position and sequences.*)
+val update_matrix : matrix -> matrix
---
+++ song.mli
@@ -0,0 +1,27 @@
+(**
+   Representation of static song data.
+
+   This module represents the data stored in song files. It handles loading of 
+   that data from JSON as well as querying the data.
+*)
+
+(** The abstract type of values representing songs. *)
+type t
+
+type difficulty = Easy | Med | Hard
+
+(** [from_json j] is the adventure that [j] represents.
+    Requires: [j] is a valid JSON adventure representation. *)
+val from_json : Yojson.Basic.t -> t
+
+(** [bpm t] is the beats per minute of the song. *)
+val bpm : t -> int
+
+(** [difficulty t] is the difficulty of the song. *)
+val difficulty : t -> difficulty 
+
+(** [song_name t] is the name of the song. *)
+val song_name : t -> string
+
+(** [music_file t] is the file that has the music. *)
+val music_file : t -> string
