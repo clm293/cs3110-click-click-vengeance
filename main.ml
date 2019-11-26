027024031a09000562cd6108cbd2889a01e37d44
Crystal <crystaltang77@gmail.com>
2019-11-26 10:42:04 -0500

    win but not working

--- main.ml
+++ main.ml
@@ -106,6 +106,22 @@ and lose s =
           then close_graph ()
           else lose s
 
+and win s = 
+  match Graphic.win s with
+  | (play_again, quit) ->  
+    let click = (wait_next_event [Button_down]) in 
+    match play_again with 
+    | (b1x,b1y) -> let b1x1 = b1x in let b1x2 = b1x + 100 in 
+      let b1y1 = b1y in let b1y2 = b1y + 75 in 
+      if button_clicked b1x1 b1x2 b1y1 b1y2 click 
+      then main ()
+      else match quit with 
+        | (b2x,b2y) -> let b2x1 = b2x in let b2x2 = b2x + 100 in 
+          let b2y1 = b2y in let b2y2 = b2y + 75 in 
+          if button_clicked b2x1 b2x2 b2y1 b2y2 click
+          then close_graph ()
+          else win s
+
 and start_window s = 
   match Graphic.start_window s with
   | (x,y) -> 
